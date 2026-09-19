02d15d9640f7dd91fa3c408673fe34ab75479457
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-19 16:40:35 +0200

    Fix direct project page rendering

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
+import { h } from 'vue'
 import ProjectBackLink from './ProjectBackLink.vue'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('ProjectBackLink', ProjectBackLink)
-  },
-  Layout: ProjectBackLink
+  Layout: () =>
+    h(DefaultTheme.Layout, null, {
+      'doc-before': () => h(ProjectBackLink)
+    })
 } satisfies Theme
