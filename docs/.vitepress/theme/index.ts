328aee996e00cbc67257b77284972637d669fdd3
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-23 14:51:12 +0200

    Fix duplicate docs layout on mobile

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
-import { h } from 'vue'
 import ProjectBackLink from './ProjectBackLink.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () =>
-    h(DefaultTheme.Layout, null, {
-      'doc-before': () => h(ProjectBackLink)
-    })
+  Layout: ProjectBackLink
 } satisfies Theme
